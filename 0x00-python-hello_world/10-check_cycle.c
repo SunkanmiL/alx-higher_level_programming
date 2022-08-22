8685590aa3c4b12f29a1717d6d8d7b1d7cdf94a5
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-08-22 12:05:19 -0700

    linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,17 +7,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *temp1 = NULL, *temp2 = NULL;
+	listint_t *s1 = NULL, *s2 = NULL;
 
-	temp1 = list;
-	temp2 = list;
-
-	while (list)
+	s1 = s2 = list;
+	while (list && s1 && s2 && s1->next && s2->next)
 	{
-		temp2 = temp2->next;
-		if (!temp1 || !temp2)
+		s1 = s1->next;
+		s2 = s2->next->next;
+		if (!s2 || !s1)
 			return (0);
-		if (temp2 == temp1)
+		if (s2->next == s1)
 			return (1);
 	}
 	return (0);
--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -9,18 +9,19 @@
 
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
-    return (n);
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
+
+	return (n);
 }
 
 /**
@@ -29,19 +30,19 @@ size_t print_listint(const listint_t *h)
  * @n: integer to be included in node
  * Return: address of the new element or NULL if it fails
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
-    return (new);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
 
 /**
@@ -49,15 +50,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
  * @head: pointer to list to be freed
  * Return: void
  */
-
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
