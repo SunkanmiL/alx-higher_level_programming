b6e37f63f9c6a8feae1d12e3f950c5817994ec75
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-08-25 11:58:00 -0700

    handles basic operations

--- 0x02-python-import_modules/tests/100-my_calculator.py
+++ 0x02-python-import_modules/tests/100-my_calculator.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
      imports all functions from the file calculator_1.py
      and handles basic operations.
     """
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
 
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
