014d7188e6ee07238a9000fe95b0903b02e0b028
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-08-31 20:48:37 -0700

    Only different

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,5 @@ def only_diff_elements(set_1, set_2):
     """
     returns a set of all elements present in only one set
     """
-     new_set = (set_1 | set_2) - (set_1 & set_2)
+    new_set = set_1 ^ set_2
     return new_set
