a6a5d8db0b54e1747471eef0d394fd29de4d3140
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-08-31 21:10:43 -0700

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """
+    converts a Roman numeral to an integer
+    """
+    if not isinstance(roman_string, str) or not roman_string:
+        return 0
+    hashMap = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    arr = [hashMap[key] for key in roman_string]
+    num = arr[-1]
+    for idx in range(len(arr)-1, 0, -1):
+        if arr[idx] > arr[idx-1]:
+            num -= arr[idx-1]
+        else:
+            num += arr[idx-1]
+    return num
