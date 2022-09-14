05b0d8fb1cdca3f7ee7e7a795da1b51e440bc9af
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-13 20:37:13 -0700

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
+"""Define a class Square"""
 
 class Square:
-    """Empty class Square that defines a square
-    Attributes:
-        empty
-    """
+    """Represent a square"""
     pass
