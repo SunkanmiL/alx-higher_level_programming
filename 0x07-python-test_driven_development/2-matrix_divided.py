f6bd3c988a745015cfd3f16c756effac5d6b2bc4
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-20 22:28:35 -0700

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,33 +14,20 @@ def matrix_divided(matrix, div):
     Returns:
         A new matrix representing the result of the division
     """
-if type(div) not in [int, float] or div != div or\
-            abs(div) > 1.7976931348623158e+308:
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
-        return matrix
-    elif div == 0:
+
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-        return matrix
 
-    if type(matrix) is list:
-        new_matrix = [x[:] for x in matrix]
-        for i in range(len(matrix)):
-            if i <= len(matrix) - 2 and len(matrix[i]) != len(matrix[i + 1]):
-                raise TypeError("Each row of the matrix must have the same" +
-                                " size")
-                return matrix
-            for j in range(len(matrix[i])):
-                if type(matrix[i][j]) not in [int, float] or\
-                        matrix[i][j] != matrix[i][j] or\
-                        abs(matrix[i][j]) > 1.7976931348623158e+308:
-                    raise TypeError("matrix must be a matrix (list of lists)" +
-                                    " of integers/floats")
-                    return matrix
-                else:
-                    new_matrix[i][j] = round(matrix[i][j] / div, 2)
-    else:
-        raise TypeError("matrix must be a matrix (list of lists)" +
-                        " of integers/floats")
-        return matrix
-
-    return new_matrix
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
