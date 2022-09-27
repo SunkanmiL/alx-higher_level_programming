9cf28d417a932d0a77305266e86d2e7b84c5ff0c
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-27 06:22:16 -0700

    Added README.md and 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a function that reads a text file (UTF8) and prints to stdout"""
+
+
+def read_file(filename=""):
+    """reads UTF8 file and prints to stdout
+
+    Args:
+        filename (str): file to be read
+        """
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
