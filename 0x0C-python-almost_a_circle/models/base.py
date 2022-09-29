07e23b5c7e8b127689041237164b9d8717ea1a52
SunkanmL <oluwamuyiwasodiq@gmail.com>
2022-09-29 05:24:11 -0700

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+"""This module contains a class called Base"""
+import json
+import csv
+import turtle
+
+
+class Base:
+    """Base class for upcoming classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation of objects to a file"""
+        if list_objs is None:
+            list_dict = []
+        else:
+            list_dict = []
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of JSON string representation"""
+        json_list = []
+        if json_string is None or len(json_string) == 0:
+            return json_list
+        else:
+            json_list = (json.loads(json_string))
+            return json_list
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance by processing a dictionary"""
+        if cls.__name__ == 'Base':
+            return
+        elif cls.__name__ == 'Square':
+            dummy = cls(1)
+        elif cls.__name__ == 'Rectangle':
+            dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        try:
+            with open(cls.__name__ + ".json", "r") as f:
+                return [cls.create(**dictionary) for
+                        dictionary in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """save to csv file"""
+        filename = cls.__name__ + ".csv"
+        content = ""
+        text = []
+        if list_objs is not None:
+            content += ','.join(list_objs[0].to_dictionary().keys())
+            content += '\n'
+            for lst in list_objs:
+                content += ','.join(
+                    map(str, lst.to_dictionary().values())
+                )
+                content += '\n'
+
+        with open(filename, mode="w", encoding="utf-8") as f:
+            return f.write(content)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """load from csv"""
+        filename = cls.__name__ + ".csv"
+        object_created = []
+
+        with open(filename, 'r') as f:
+            header = f.readline().replace('\n', '').split(',')
+            for el in f.readlines():
+                values = map(int, el.replace('\n', '').split(','))
+                data = dict(zip(header, values))
+                object_created.append(cls.create(**data))
+        return object_created
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """Opens a window and draws Rectangles and Squares"""
+        draw = turtle.Turtle()
+
+        for rectangle in list_rectangles:
+            draw.showturtle()
+            draw.up()
+            draw.goto(rectangle.x, rectangle.y)
+            draw.down()
+            for i in range(2):
+                draw.forward(rectangle.width)
+                draw.left(90)
+                draw.forward(rectangle.height)
+                draw.left(90)
+            draw.hideturtle()
+
+        for square in list_squares:
+            draw.showturtle()
+            draw.up()
+            draw.goto(square.x, square.y)
+            draw.down()
+            for i in range(2):
+                draw.forward(square.width)
+                draw.left(90)
+                draw.forward(square.height)
+                draw.left(90)
+            draw.hideturtle()
