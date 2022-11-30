9d63f1f6048488b6fb4f2b5afe4240385418c9d5
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2022-11-30 04:39:26 +0000

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,4 +1,5 @@
-t dict = require('./101-data.js').dict;
+#!/usr/bin/node
+const dict = require('./101-data.js').dict;
 let newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
