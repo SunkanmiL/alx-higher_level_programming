4f30e8534f6223f154284e8eed75f2d35e9a2a32
Oluwamuyiwa Lawal <93699548+SunkanmiL@users.noreply.github.com>
2022-12-05 04:45:04 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from relationship_city import Base, City
 
+Base = declarative_base()
+
 
 class State(Base):
     """Represents a state for a MySQL database
@@ -18,5 +20,4 @@ class State(Base):
     __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", backref="state", cascade="all, delete")
