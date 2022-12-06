6edf82944aca364107777bef803e34f7c2efc70a
Oluwamuyiwa <oluwamuyiwasodiq@gmail.com>
2022-12-06 03:04:17 -0800

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" python file that contains the class definition of a State
+"""
+    Python file that contains the class definition of a State
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
