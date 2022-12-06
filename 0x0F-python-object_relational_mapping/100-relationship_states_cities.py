42de71ed4a1f655ad2987a3dfea777264b6b7844
Oluwamuyiwa <oluwamuyiwasodiq@gmail.com>
2022-12-06 05:19:58 -0800

    Added 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -7,24 +7,15 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from relationship_state import State
-from relationship_city import City
+from relationship_city import Base, City
 
-if __name__ == '__main__':
-    args = sys.argv
-    if len(args) != 4:
-        print("Usage: {} username password database_name".format(args[0]))
-        exit(1)
-    username = args[1]
-    password = args[2]
-    data = args[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(username, password, data))
-    # create custom session object class from database engine
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    # create instance of new custom session class
     session = Session()
-    california = State(name="California")
-    california.cities = [City(name="San Francisco")]
-    session.add(california)
+
+    session.add(City(name="San Francisco", state=State(name="California")))
     session.commit()
-    session.close()
