73610fe6c9a7bd1bb4f183a5dda7d142c62c3550
Oluwamuyiwa Lawal <oluwamuyiwasodiq@gmail.com>
2023-01-17 07:14:47 -0800

    Added 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const movieID = parseInt(process.argv[2]);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieID;
+let data;
+
 request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
